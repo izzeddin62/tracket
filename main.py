9f107fe7df5b7afcbfbedac060a6b5463b72816f
Freedisch <freeproduc@gamil.com>
2022-07-14 15:08:52 +0200

    main page

---
+++ main.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def main_page():
+    print("HELLO WELCOME TO TRACKET\n")
+    print("Choose an option\n")
+    print("1.View my expense\n")
+    print("2.Add new expense\n")
+    print("3.Total expenses\n")
+    option = input("What do you want to do?")
+    return option
+
+print(main_page())
