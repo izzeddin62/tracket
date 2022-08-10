ecd179055cdfa67e0e2f36a32027fcf8fdc5e139
Freedisch <82499435+Freedisch@users.noreply.github.com>
2022-08-10 20:59:28 +0200

    Expenses class

--- expenselist.py
+++ expenselist.py
@@ -6,3 +6,17 @@ class expense:
         self.value = value
         self.id = str(uuid4)
     
+
+    """ getter method """
+    def get_name(self):
+        return self.name
+
+    def get_value(self):
+        return self.value
+        
+    """ setter method """
+    def set_value(self, x):
+        self.name = x
+    
+    def set_name(self, y):
+        self.value = y
